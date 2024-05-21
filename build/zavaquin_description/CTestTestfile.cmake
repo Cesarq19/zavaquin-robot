0469b4019417a2d134be587ca02185bc3636b660
Cesarq19 <cepaquin@espol.edu.ec>
2024-05-20 23:09:20 -0500

    correccion estructura

--- build/zavaquin_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonnysamz/zavaquin-robot/src/zavaquin_description
-# Build directory: /home/jonnysamz/zavaquin-robot/build/zavaquin_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
